ce68809dbbda0c1791ceb9d3fa5b3c015fc28952
Edwin Lock <edwinlock@gmail.com>
2024-02-26 15:20:50 +0000

    tidied up

--- src/NetworkTrading.jl
+++ src/NetworkTrading.jl
@@ -1,89 +1,13 @@
 module NetworkTrading
 
-struct Market
-    n::Int  # number of agents
-    m::Int  # number of trades
-    Ω::Vector{Tuple{Int,Int}}  # list of trades given as ordered pairs (seller, buyer)
-    trades::Vector{Set{Int}}  # trades of agent i for each agent
-    offers::Vector{Dict{Int,Int}}  # for each agent, dict mapping trades to offer
-    unsatisfied::Set{Int}  # set of unsatisfied agents in the market
-    valuation::Vector{Function}
-    utility::Vector{Function}
-    demand::Vector{Function}
-
-    # Inner constructor
-    function Market(Ω; offers, valuation, demand)
-        m = length(Ω)  # number of trades
-        n = length(valuation)  # number of agents
-        # Check that input is consistent
-        length(demand) ≠ length(valuation) && error("Valuation and demand must have same length.")
-        ([t[1] for t in Ω] ∪ [t[2] for t in Ω]) ⊆ 1:n || error("Agents specified in trade must be subset of 1:length(types).")
-        any(t[1]==t[2] for t in Ω) && error("Network cannot contain loops (trades from an agent to itself).")
-        # Construct data structures
-        trades_per_agent = [associated_trades(i, Ω) for i ∈ 1:n]
-        # offers = [Dict(ω => rand(MINVAL:MAXVAL) for ω ∈ trades_per_agent[i]) for i ∈ 1:n]
-        unsatisfied = Set(1:n)
-        utility = [generate_utility(i, Ω, valuation[i]) for i ∈ 1:n]
-        new(n, m, Ω, trades_per_agent, offers, unsatisfied, valuation, utility, demand)
-    end
-end
-
-### Utilities
-
-"""
-Determine whether agent i is seller/buyer of trade ω in list of trades Ω.
-"""
-isseller(i, ω, Ω) = i == Ω[ω][1]
-isbuyer(i, ω, Ω) = i == Ω[ω][2]
-
-
-"""
-Define characteristic function that returns 1 if agent i is buyer in ω, -1
-if i is seller in ω, and 0 otherwise.
-"""
-function χ(i, ω, Ω)
-    isbuyer(i, ω, Ω) && return 1
-    isseller(i, ω, Ω) && return -1
-    return 0
-end
-
-
-"""
-Compute all trades in Ω that are associated with agent i.
-"""
-associated_trades(i, Ω) = Set(ω for ω ∈ 1:length(Ω) if i ∈ Ω[ω])
-
-"""
-Compute incoming trades in Ω that are associated with agent i.
-"""
-incoming_trades(i, Ω) = Set(ω for ω ∈ 1:length(Ω) if isbuyer(i, ω, Ω))
-
-"""
-Compute outgoing trades in Ω that are associated with agent i.
-"""
-outgoing_trades(i, Ω) = Set(ω for ω ∈ 1:length(Ω) if isseller(i, ω, Ω))
-
-"""
-Compute counterpart of agent i for trade ω in list of trades Ω.
-"""
-function counterpart(i, ω, Ω)
-    trade = Ω[ω]
-    if trade[1] == i
-        return trade[2]
-    elseif trade[2] == i
-        return trade[1]
-    else
-        error("Agent i must be involved in trade ω.")
-    end
-end
-
+include("markets.jl")
 include("dynamic.jl")
 include("preferences.jl")
 include("plotting.jl")
-include("markets.jl")
+
 
 export Market, isseller, isbuyer, associated_trades, incoming_trades, outgoing_trades, χ, counterpart
-export neighbouring_offers, active, welfare, compute_offers, best_response!, dynamic
+export neighbouring_offers, active, welfare, updated_offers, best_response!, dynamic
 export generate_intermediary_demand, generate_intermediary_valuation, generate_utility, generate_unit_valuation, generate_unit_demand
 export plot_offers, plot_satisfied, plot_welfare
 export BipartiteUnitMarket, RandomBipartiteUnitMarket, IntermediaryUnitMarket, RandomIntermediaryUnitMarket
--- src/dynamic.jl
+++ src/dynamic.jl
@@ -1,79 +1,8 @@
+"""
+Implementation of best response dynamic.
+"""
+
 using ProgressMeter
-
-### Implementation of dynamic
-
-"""
-Retrieve the offers of agent i's neighbours.
-"""
-function neighbouring_offers(i::Int, offers, market::Market)::Dict{Int,Int}
-    prices = Dict{Int,Int}()
-    for ω ∈ market.trades[i]  # Set p[ω] to the offer of counterpart of trade    
-        j = counterpart(i, ω, market.Ω)
-        prices[ω] = offers[j][ω]
-    end
-    return prices
-end
-neighbouring_offers(i::Int, market::Market) = neighbouring_offers(i, market.offers, market)
-
-
-"""
-Compute active trades and prices. Returns prices and set of trades (p, Ψ).
-"""
-function active(offers, market::Market)
-    # Compute trades for which both offers agree, and their prices.
-    Ψ = Set{Int}()
-    p = Dict{Int,Int}()
-    for ω ∈ 1:market.m
-        buyer, seller = market.Ω[ω]
-        if offers[buyer][ω] == offers[seller][ω]
-            push!(Ψ, ω)
-            p[ω] = offers[buyer][ω]
-        end
-    end
-    return p, Ψ
-end
-
-
-"""
-Compute welfare (aggregate utility) of market with specified offers.
-"""
-function welfare(offers, market::Market)
-    p, Ψ = active(offers, market::Market)    
-    return sum(market.utility[i](p,Ψ) for i ∈ 1:market.n;init=0)
-end
-welfare(market::Market) = welfare(market.offers, market)
-
-
-"""
-Compute new offers of agent i given market prices p and demanded bundle Ψ.
-"""
-function compute_offers(i, p, Ψ, market::Market)
-    function newoffer(ω)
-        ω ∈ Ψ && return p[ω]
-        χ = isbuyer(i, ω, market.Ω) ? 1 : -1
-        return p[ω] - χ
-    end
-    return Dict(ω => newoffer(ω) for ω ∈ market.trades[i])
-end
-
-
-"""
-Perform best response for agent i. Updates offers of the agent and
-the set of unsatisfied neighbours of the market.
-"""
-function best_response!(i, market::Market)
-    p = neighbouring_offers(i, market)
-    Ψ = market.demand[i](p)
-    newoffers = compute_offers(i, p, Ψ, market)
-    newly_unsatisfied = Set(
-        counterpart(i, ω, market.Ω) for ω ∈ market.trades[i]
-            if market.offers[i][ω] ≠ newoffers[ω]
-    )
-    market.offers[i] = newoffers
-    union!(market.unsatisfied, newly_unsatisfied)
-    delete!(market.unsatisfied, i)  # agent i is now satisfied
-    return nothing
-end
 
 
 """
@@ -96,4 +25,49 @@ function dynamic(market)
         step += 1
     end
     return step, data
+end
+
+
+"""
+Perform best response for agent i. Updates offers of the agent and
+the set of unsatisfied neighbours of the market.
+"""
+function best_response!(i, market::Market)
+    p = neighbouring_offers(i, market)
+    Ψ = market.demand[i](p)
+    newoffers = updated_offers(i, p, Ψ, market)
+    newly_unsatisfied = Set(
+        counterpart(i, ω, market.Ω) for ω ∈ market.trades[i]
+            if market.offers[i][ω] ≠ newoffers[ω]
+    )
+    market.offers[i] = newoffers
+    union!(market.unsatisfied, newly_unsatisfied)
+    delete!(market.unsatisfied, i)  # agent i is now satisfied
+    return nothing
+end
+
+
+"""
+Retrieve the offers of agent i's neighbours.
+"""
+function neighbouring_offers(i::Int, offers, market::Market)::Dict{Int,Int}
+    prices = Dict{Int,Int}()
+    for ω ∈ market.trades[i]  # Set p[ω] to the offer of counterpart of trade    
+        j = counterpart(i, ω, market.Ω)
+        prices[ω] = offers[j][ω]
+    end
+    return prices
+end
+neighbouring_offers(i::Int, market::Market) = neighbouring_offers(i, market.offers, market)
+
+
+"""
+Compute updated offers of agent i given market prices p and demanded bundle Ψ.
+"""
+function updated_offers(i, p, Ψ, market::Market)
+    function newoffer(ω)
+        ω ∈ Ψ && return p[ω]
+        return p[ω] - χ(i, ω, market.Ω)
+    end
+    return Dict(ω => newoffer(ω) for ω ∈ market.trades[i])
 end
