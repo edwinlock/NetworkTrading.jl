39dd276fb659eb4a447ac4ffab02f2904cefc195
Edwin Lock <mail@edwinlock.com>
2025-04-17 21:53:29 +0100

    added leximin, leximax, and var min optimisation problems

--- src/optimisation.jl
+++ src/optimisation.jl
@@ -1,6 +1,6 @@
 using JuMP, Gurobi
 using Combinatorics
-
+using Revise
 
 function simple_test()
     model = Model(Gurobi.Optimizer)
@@ -13,9 +13,11 @@ end
 
 
 """
-Create and solve model to find a minimum variance core imputation
+Create model to find a minimum variance core imputation
 of the cooperative market game defined by agents 1 to `n` and
 welfare function `w`.
+
+Returns model and core imputation variables x.
 
 The convex optimisation program is:
 
@@ -23,38 +25,46 @@ min     sum(x_i^2 for i ∈ 1:n)
 s.t.    sum(x_i for i ∈ 1:n) == w(1:n)
         sum(x_i for i ∈ C) ≥ w(C), for every C ⊆ 1:n.
 
-Note: assumes that w(C) is defined for each C ⊆ I!
+Note: assumes that w(C) is defined for each C ⊆ 1:n!
 """
-function min_variance(n, w)
+function minvar_model(n::Int, w)
+    grand_coalition = collect(1:n)
     proper_subsets = collect(powerset(1:n, 1, n-1))
     model = Model(Gurobi.Optimizer)
     @variable(model, x[1:n])
-    @constraint(model, eq, sum(x) == w(1:n))
+    @constraint(model, eq, sum(x) == w(grand_coalition))
     @constraint(model, ineq[C ∈ proper_subsets], sum(x[C]) ≥ w(C))
     @objective(model, Min, sum(x[i]^2 for i ∈ 1:n))
-    optimize!(model)
-    return value.(x)
+    return model, x
 end
 
 
-# ad-hoc test of min-variance function:
-n = 3
-function w(C::Vector{Int})
-    length(C) ≤ 1 && return 0
-    C == [1,2] && return 0
-    C == [1,3] && return 6
-    C == [2,3] && return 3
-    C == [1,2,3] && return 8
-    return nothing
-end
 
-min_variance(I, w)
+"""
+Create model to find a leximin core imputation of the cooperative
+market game defined by agents 1 to `n` and welfare function `w`.
 
+Returns model and core imputation variables x.
 
+The convex optimisation program is:
 
-function leximin(n, w)
-    ε = 0.01  # TODO: compute correct value
+leximax y
+s.t.    sum(x_i for i ∈ 1:n) == w(1:n)
+        sum(x_i for i ∈ C) ≥ w(C), for every C ⊆ 1:n.
+        x_i ≥ 0, for all i ∈ 1:n
+        P is a permutation matrix
+        y = P x
+        y is sorted in ascending order
 
+Note: assumes that w(C) is defined for each C ⊆ 1:n!
+"""
+function leximin_model(n, w)
+    # First we define a constant for the objective function, i.e., M = 1 / ε.
+    # This constant should probably depend on n and the magnitude of the values in w.
+    # For now, we set it to a fairly number. TODO: improve.
+    M = 500
+
+    grand_coalition = collect(1:n)
     proper_subsets = collect(powerset(1:n, 1, n-1))
     model = Model(Gurobi.Optimizer)
     
@@ -64,7 +74,7 @@ function leximin(n, w)
     @variable(model, P[1:n, 1:n], Bin)
 
     ## Define constraints
-    @constraint(model, eq, sum(x) == w(1:n))
+    @constraint(model, eq, sum(x) == w(grand_coalition))
     @constraint(model, ineq[C ∈ proper_subsets], sum(x[C]) ≥ w(C))
     # Ensure that P is a permutation matrix
     @constraint(model, col[i ∈ 1:n], sum(P[:,i]) == 1)
@@ -75,6 +85,74 @@ function leximin(n, w)
     @constraint(model, sorting[i ∈ 1:n-1], y[i] ≤ y[i+1])
 
     ## Define objective
-    @objective(model, Min, sum(x[i]^2 for i ∈ 1:n)) # TODO!
-    optimize!(model)
+    @objective(model, Max, sum(M^(n-i) * y[i] for i ∈ 1:n))
+    
+    return model, x, y
 end
+
+
+"""
+Create model to find a leximax core imputation of the cooperative
+market game defined by agents 1 to `n` and welfare function `w`.
+
+Returns model and core imputation variables x.
+
+The convex optimisation program is:
+
+leximin y
+s.t.    sum(x_i for i ∈ 1:n) == w(1:n)
+        sum(x_i for i ∈ C) ≥ w(C), for every C ⊆ 1:n.
+        x_i ≥ 0, for all i ∈ 1:n
+        P is a permutation matrix
+        y = P x
+        y is sorted in descending order
+
+Note: assumes that w(C) is defined for each C ⊆ 1:n!
+"""
+function leximax_model(n::Int, w)
+    # First we define a constant for the objective function, i.e., M = 1 / ε.
+    # This constant should probably depend on n and the magnitude of the values in w.
+    # For now, we set it to a fairly number. TODO: improve.
+    M = 500
+    model, x, y = leximin_model(n, w)
+    # Change the objective to leximax
+    @objective(model, Min, sum(M^(i) * y[i] for i ∈ 1:n))
+    return model, x
+end
+
+
+function find_optimal_core_imputation(n::Int, w, objective::Symbol)
+    # Define the model based on the objective specified
+    if objective == :leximin
+        model_fn = leximin_model
+    elseif objective == :leximax
+        model_fn = leximax_model
+    elseif objective == :min_variance
+        model_fn = minvar_model
+    else
+        error("Unknown objective function: $(objective)")
+    end
+    # Create model and core imputation variables
+    model, x = model_fn(n, w)
+    # Solve model and return result
+    optimize!(model)
+    @info "Optimal core imputation: $(value.(x))"
+    return value.(x)
+end
+
+
+
+########### ad-hoc test of min-variance function:
+n = 3
+function w(C::Vector{Int})
+    length(C) ≤ 1 && return 0
+    C == [1,2] && return 0
+    C == [1,3] && return 6
+    C == [2,3] && return 3
+    C == [1,2,3] && return 8
+    return nothing
+end
+
+minvar_sol = find_optimal_core_imputation(n, w, :min_variance)
+leximin_sol = find_optimal_core_imputation(n, w, :leximin)
+leximax_sol = find_optimal_core_imputation(n, w, :leximax)
