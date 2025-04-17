7fe246bc90201f3b6ac0d0986b52576581ad23f9
Edwin Lock <mail@edwinlock.com>
2025-04-17 20:27:43 +0100

    Started the leximin model formulation. It's still missing the correct objective.

--- src/optimisation.jl
+++ src/optimisation.jl
@@ -14,31 +14,31 @@ end
 
 """
 Create and solve model to find a minimum variance core imputation
-of the cooperative market game defined by agents `I` and welfare
-function `w`.
+of the cooperative market game defined by agents 1 to `n` and
+welfare function `w`.
 
 The convex optimisation program is:
 
-min     sum(x_i^2 for i ∈ I)
-s.t.    sum(x_i for i ∈ I) == w(I)
-        sum(x_i for i ∈ C) ≥ w(C), for every C ⊆ I.
+min     sum(x_i^2 for i ∈ 1:n)
+s.t.    sum(x_i for i ∈ 1:n) == w(1:n)
+        sum(x_i for i ∈ C) ≥ w(C), for every C ⊆ 1:n.
 
 Note: assumes that w(C) is defined for each C ⊆ I!
 """
-function min_variance(I, w)
-    proper_subsets = collect(powerset(I, 1, length(I)-1))
+function min_variance(n, w)
+    proper_subsets = collect(powerset(1:n, 1, n-1))
     model = Model(Gurobi.Optimizer)
-    @variable(model, x[I])
-    @constraint(model, eq, sum(x) == w(I))
+    @variable(model, x[1:n])
+    @constraint(model, eq, sum(x) == w(1:n))
     @constraint(model, ineq[C ∈ proper_subsets], sum(x[C]) ≥ w(C))
-    @objective(model, Min, sum(x[i]^2 for i ∈ I))
+    @objective(model, Min, sum(x[i]^2 for i ∈ 1:n))
     optimize!(model)
     return value.(x)
 end
 
 
 # ad-hoc test of min-variance function:
-I = [1,2,3]
+n = 3
 function w(C::Vector{Int})
     length(C) ≤ 1 && return 0
     C == [1,2] && return 0
@@ -49,3 +49,32 @@ function w(C::Vector{Int})
 end
 
 min_variance(I, w)
+
+
+
+function leximin(n, w)
+    ε = 0.01  # TODO: compute correct value
+
+    proper_subsets = collect(powerset(1:n, 1, n-1))
+    model = Model(Gurobi.Optimizer)
+    
+    ## Define variables
+    @variable(model, x[1:n] ≥ 0)
+    @variable(model, y[1:n] ≥ 0)
+    @variable(model, P[1:n, 1:n], Bin)
+
+    ## Define constraints
+    @constraint(model, eq, sum(x) == w(1:n))
+    @constraint(model, ineq[C ∈ proper_subsets], sum(x[C]) ≥ w(C))
+    # Ensure that P is a permutation matrix
+    @constraint(model, col[i ∈ 1:n], sum(P[:,i]) == 1)
+    @constraint(model, row[i ∈ 1:n], sum(P[i,:]) == 1)
+    # Define relation y == P x
+    @constraint(model, P * x == y)  # change to .== if Gurobi can't handle ==
+    # Ensure that y is sorted in ascending order
+    @constraint(model, sorting[i ∈ 1:n-1], y[i] ≤ y[i+1])
+
+    ## Define objective
+    @objective(model, Min, sum(x[i]^2 for i ∈ 1:n)) # TODO!
+    optimize!(model)
+end
